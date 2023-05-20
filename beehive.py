a4ba992610686ad292008458d77720a40037b4b2
Chong Chi Yang <ccho0092@student.monash.edu>
2023-05-20 16:40:29 +0800

    Completed Tasks

--- beehive.py
+++ beehive.py
@@ -13,16 +13,37 @@ class Beehive:
     nutrient_factor: int
     volume: int = 0
 
+    def __init__(self, x, y ,z, capacity, nutrient_factor, volume):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.capacity = capacity
+        self.nutrient_factor = nutrient_factor
+        self.volume = volume
+
+    def __le__(self, other):
+        return min(self.capacity, self.volume)*self.nutrient_factor <= min(other.capacity, other.volume)*other.nutrient_factor
+
+    def __gt__(self, other):
+        return min(self.capacity, self.volume)*self.nutrient_factor > min(other.capacity, other.volume)*other.nutrient_factor
+
+
 class BeehiveSelector:
 
     def __init__(self, max_beehives: int):
-        raise NotImplementedError()
+        self.store = MaxHeap(max_beehives)
 
     def set_all_beehives(self, hive_list: list[Beehive]):
-        raise NotImplementedError()
-    
+        self.store = MaxHeap(self.capacity)
+        for hive in hive_list:
+            self.store.add(hive) # O(log n) :(
+
     def add_beehive(self, hive: Beehive):
-        raise NotImplementedError()
+        self.store.add(hive) # O(log n) :)
     
     def harvest_best_beehive(self):
-        raise NotImplementedError()
+        best: Beehive = self.store.get_max() # O(log n) :)
+        value = min(best.capacity, best.volume)
+        best.volume -= value
+        self.store.add(best) # O(log n) :)
+        return value * best.nutrient_factor
